55c8468b0df4bc7b2007cd9ffa57e1222eb79fa0
Nguyen-Dang-Ky <169076957+Nguyen-Dang-Ky@users.noreply.github.com>
2026-04-21 13:39:25 +0700

    Update IContractService.cs

--- services/Interfaces/IContractService.cs
+++ services/Interfaces/IContractService.cs
@@ -8,11 +8,11 @@ using QuanLyPhongTro.dtos;
 namespace QuanLyPhongTro.services.Interfaces
 {
     public interface IContractService
-    {
-        Task<IEnumerable<ContractResponseDto>> GetAllContractAsync();
-        Task<ContractResponseDto?> GetContractById(int id);
-        Task<ContractResponseDto> createContractAsync(CreateContractDto createDto);
-        Task<ContractResponseDto> updateContractAsync(UpdateContractDto updateDto);
-        Task<bool> deleteContractAsync(int id);
-    }
+        {
+            Task<IEnumerable<ContractResponseDto>> GetAllContractsAsync();
+            Task<ContractResponseDto?> GetContractByIdAsync(int id);
+            Task<ContractResponseDto> CreateContractAsync(CreateContractDto createDto);
+            Task<ContractResponseDto> UpdateContractAsync(int id, UpdateContractDto updateDto);
+            Task<bool> DeleteContractAsync(int id);
+        }
 }
