d7e1cf01f257b84879ba43b1472cb1badfa88760
Nguyen-Dang-Ky <169076957+Nguyen-Dang-Ky@users.noreply.github.com>
2026-04-21 13:28:00 +0700

    Update IContractService.cs

--- services/Interfaces/IContractService.cs
+++ services/Interfaces/IContractService.cs
@@ -1,1 +1,18 @@
-Task<bool> DeleteRoomAsync (int id);
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using QuanLyPhongTro.dtos.ContractDtos;
+using QuanLyPhongTro.dtos;
+
+namespace QuanLyPhongTro.services.Interfaces
+{
+    public interface ICustomerService
+    {
+        Task<IEnumerable<CustomerResponseDto>> GetAllCustomerAsync();
+        Task<CustomerResponseDto?> GetCustomerById(int id);
+        Task<CustomerResponseDto> CreateCustomerAsync(CustomerCreateDto createdto);
+        Task<CustomerResponseDto> UpdateCustomerAsync(CustomerUpdateDto updatedto);
+        Task<bool> DeleteCustomerAsync(int id); 
+    }
+}
